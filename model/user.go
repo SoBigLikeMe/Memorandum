3f78329c510a6bbb8053f80c33ff3fac70cc94c3
N0tDifficult <2097639788@qq.com>
2022-09-25 16:51:31 +0800

    加了点注释

--- model/user.go
+++ model/user.go
@@ -5,14 +5,14 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-//建表User
+// User 建表User
 type User struct {
 	gorm.Model
 	UserName       string `gorm:"unique" json:"userName,omitempty"`
 	PasswordDigest string //密文
 }
 
-//加密
+// SetPassword 加密
 func (user *User) SetPassword(password string) error {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
@@ -22,7 +22,7 @@ func (user *User) SetPassword(password string) error {
 	return nil
 }
 
-//验证密码
+// CheckPassword 验证密码
 func (user *User) CheckPassword(password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordDigest), []byte(password))
 	return err == nil
